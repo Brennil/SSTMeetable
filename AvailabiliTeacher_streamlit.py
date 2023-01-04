5b5c1e224bc7f085000dc853eabd7d7f4ac95e80
Jovita Tang <47584623+Brennil@users.noreply.github.com>
2023-01-04 21:09:26 +0800

    Update AvailabiliTeacher_streamlit.py

--- AvailabiliTeacher_streamlit.py
+++ AvailabiliTeacher_streamlit.py
@@ -155,7 +155,7 @@ else:
                     if val not in x[key]:
                         all_avail[key].remove(val)
 
-    st.write("\n")
+    st.write("\n\n\n")
     st.write("**Even Week**")
     time_converter(all_avail)
     st.write("\n")
