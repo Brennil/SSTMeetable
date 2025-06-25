8bd43e8ca9690ac82316618ec2a0f8b2112f34c1
Jovita Tang <47584623+Brennil@users.noreply.github.com>
2025-06-26 00:33:57 +0800

    Updated database for 2025 T3-4 to Ver 2.4a

--- Meetable.py
+++ Meetable.py
@@ -13,7 +13,7 @@ Previously AvailabiliTeacher
 (Ver 3.3)
 '''
 
-st.markdown("This app is currently available for: :red[**Term 2 2025**] (Ver2, 21 Mar 2025)")
+st.markdown("This app is currently available for: :red[**Term 3-4 2025**] (Ver2.4a, 26 Jun 2025)")
 
 '''
 
@@ -40,7 +40,7 @@ db = dict()
 
 def open_db(filename):
     spread = gc.open(filename)
-    worksheet = spread.worksheet("2025T2") #CHANGE THIS WHEN CHANGING TERM DATABASES!!!
+    worksheet = spread.worksheet("2025T3-4") #CHANGE THIS WHEN CHANGING TERM DATABASES!!!
     csvdb = worksheet.get_all_values()
     
     db['Monday'] = defaultdict()
