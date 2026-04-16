b0e3fe3d85c48190c8f2ef6845637cf504a15a40
Jovita Tang <47584623+Brennil@users.noreply.github.com>
2026-04-16 08:16:56 +0800

    Update Meetable.py

--- Meetable.py
+++ Meetable.py
@@ -153,7 +153,6 @@ def time_converter(all_avail):
     # CSS to inject contained in a string
     hide_table_row_index = """
             <style>
-            thead tr th:first-child {display:none}
             tbody th {display:none}
             </style>
             """
